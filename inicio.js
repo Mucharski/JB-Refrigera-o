6ac666ef2488ed19500481cd513fcd4302aec15c
Mucharski <igor.m99@hotmail.com>
2018-10-29 12:12:53 -0300

    first commit

---
+++ inicio.js
@@ -0,0 +1,29 @@
+$(document).ready(function () {
+
+    var _0x9cd6 = ["\x76\x61\x6C",
+        "\x23\x69\x6E\x4C\x6F\x67\x69\x6E",
+        "\x23\x69\x6E\x53\x65\x6E\x68\x61",
+        "\x61\x64\x6D\x69\x6E", "\x6A\x62\x32\x33\x30\x33",
+        "\x68\x72\x65\x66", "\x6C\x6F\x63\x61\x74\x69\x6F\x6E",
+        "\x63\x61\x64\x61\x73\x74\x72\x6F\x5F\x64\x65\x5F\x70\x72\x6F\x64\x75\x74\x6F\x73\x2F\x69\x6E\x64\x65\x78\x2E\x68\x74\x6D\x6C",
+        "\x4C\x6F\x67\x69\x6E\x20\x65\x2F\x6F\x75\x20\x73\x65\x6E\x68\x61\x20\x6E\xE3\x6F\x20\x63\x6F\x6E\x66\x65\x72\x65\x6D",
+        "\x66\x6F\x63\x75\x73", "\x63\x6C\x69\x63\x6B", "\x23\x62\x74\x49\x72"];
+
+    $(_0x9cd6[11])[_0x9cd6[10]]
+        (function () {
+            const _0xf05bx1 = $(_0x9cd6[1])[_0x9cd6[0]]();
+            const _0xf05bx2 = $(_0x9cd6[2])[_0x9cd6[0]]();
+            if (_0xf05bx1 === _0x9cd6[3] && _0xf05bx2 === _0x9cd6[4]) {
+                window[_0x9cd6[6]][_0x9cd6[5]] = _0x9cd6[7]
+            } else {
+                alert(_0x9cd6[8]); $(_0x9cd6[1])[_0x9cd6[9]]();
+                return
+            }
+
+        }); // função tá obsfucada porque tem a senha
+
+    $(document).keypress(function (e) {
+        if (e.which == 13) $("#btIr").click();
+    }); // função para o enter acionar o botão de login
+
+}); // final 
