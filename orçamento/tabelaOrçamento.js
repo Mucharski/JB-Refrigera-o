dd3668ccc5db7ab9fffc52c6294720648d534c47
Mucharski <igor.m99@hotmail.com>
2018-11-08 12:06:15 -0200

    Quase pronto

--- orçamento/tabelaOrçamento.js
+++ orçamento/tabelaOrçamento.js
@@ -3,15 +3,14 @@ $(document).ready(function () {
     $("#btCadastrarC").click(function () {
 
         var nome = $("#inNome").val();
-        var CPF = $("#inCPF").val();
-        var telefone = $("#inTelefone").val();
-        var email = $("#inEmail").val();
-        var endereco = $("#inEndereco").val();
-        var numeroCasa = $("#inNumero").val();
+        var servicoS = $("#inServico").val();
+        var produto = $("#inProduto").val();
+        var orcamento = $("#inOrcamento").val();
+        var OS = Math.random() * (50000 - 1) + 1;
 
         // todos atributos da página e seus valores já estão sendo pegos
 
-        if (nome == "" || (CPF == "" || (telefone == "" || (email == "")))) {
+        if (nome == "" || (servicoS == "" || (produto == "" || (orcamento == "")))) {
             $("#algoErrado").text("* Preencha os campos corretamente").css({ 'color': 'red', 'opacity': '0.5', 'font-size': '12px' });
             return;
 
@@ -24,26 +23,26 @@ $(document).ready(function () {
 
         const coluna = document.createElement("tr"); // cria a linha inteira
 
-        $("#tabelaCliente").append(coluna); // apensa a linha criada ao id tabelaCliente
+        $("#tabelaOrcamento").append(coluna); // apensa a linha criada ao id tabelaCliente
 
         const linha = document.createElement("td"); // cria uma coluna
         linha.innerHTML = nome // coluna coloca o nome
         $(coluna).append(linha); // apensa a coluna do nome a linha criada
 
         const linha2 = document.createElement("td");
-        linha2.innerHTML = CPF
+        linha2.innerHTML = servicoS
         $(coluna).append(linha2);
 
         const linha3 = document.createElement("td");
-        linha3.innerHTML = telefone
+        linha3.innerHTML = produto
         $(coluna).append(linha3);
 
         const linha4 = document.createElement("td"); // mesmo raciocinio dos comentarios acima
-        linha4.innerHTML = email
+        linha4.innerHTML = orcamento
         $(coluna).append(linha4);
 
         const linhaExtra = document.createElement("td")
-        linhaExtra.innerHTML = endereco + " nº: " + numeroCasa
+        linhaExtra.innerHTML = OS.toFixed(0)
         $(coluna).append(linhaExtra)
 
         const linha5 = document.createElement("td"); // essa td é criada especialmente para o botão
@@ -52,7 +51,7 @@ $(document).ready(function () {
         botao.innerHTML = "Excluir &#10007;" // aqui o texto que ficará dentro dele
         botao.onclick = function () {
 
-            var certeza = confirm("Tem certeza que deseja excluir o cadastro?")
+            var certeza = confirm("Tem certeza que deseja excluir o serviço?")
             if (certeza === true) {
                 $(this).parent().parent().remove();
             } else {
@@ -74,5 +73,3 @@ $(document).ready(function () {
         window.location.href = "../index.html"
     })
 });
-
-
